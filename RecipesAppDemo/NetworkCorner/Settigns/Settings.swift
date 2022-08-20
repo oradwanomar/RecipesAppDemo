34fcf837c2ef28144765bfd3260aa4707337bb66
omarradwan <oradwan037@gmail.com>
2022-08-20 18:28:27 +0200

    create settings & update podfile

---
+++ RecipesAppDemo/NetworkCorner/Settigns/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  RecipesAppDemo
+//
+//  Created by Omar Ahmed on 20/08/2022.
+//
+
+import Foundation
+
+enum Settings {
+    
+    static let baseURL = "https://api.edamam.com/"
+    static let appKey = "a2fa85a3ffb845be35ea7f391a71f773"
+    static let appID = "d6d2e206"
+    
+}
