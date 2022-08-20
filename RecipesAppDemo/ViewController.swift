d2dbf6368bc1e06773ae7f2d59082c8f7c772b5f
omarradwan <oradwan037@gmail.com>
2022-08-20 03:57:30 +0200

    First Commit

---
+++ RecipesAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipesAppDemo
+//
+//  Created by Omar Ahmed on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RecipesAppDemoUITests/RecipesAppDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RecipesAppDemoUITestsLaunchTests.swift
+//  RecipesAppDemoUITests
+//
+//  Created by Omar Ahmed on 20/08/2022.
+//
+
+import XCTest
+
+class RecipesAppDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
