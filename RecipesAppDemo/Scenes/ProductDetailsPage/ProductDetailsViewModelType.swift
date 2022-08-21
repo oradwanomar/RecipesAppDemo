d2d06b50e93946396c64e5c245d71913991894c1
omarradwan <oradwan037@gmail.com>
2022-08-21 04:10:41 +0200

    create product details

---
+++ RecipesAppDemo/Scenes/ProductDetailsPage/ProductDetailsViewModelType.swift
@@ -0,0 +1,24 @@
+//  
+//  ProductDetailsViewModelType.swift
+//  RecipesAppDemo
+//
+//  Created by Omar Ahmed on 20/08/2022.
+//
+
+import Foundation
+
+/// ProductDetails Input & Output
+///
+typealias ProductDetailsViewModelType = ProductDetailsViewModelInput & ProductDetailsViewModelOutput
+
+/// ProductDetails ViewModel Input
+///
+protocol ProductDetailsViewModelInput {
+  
+}
+
+/// ProductDetails ViewModel Output
+///
+protocol ProductDetailsViewModelOutput {
+
+}
--- RecipesAppDemo/Scenes/ProductsPage/ProductsViewController.swift
+++ RecipesAppDemo/Scenes/ProductsPage/ProductsViewController.swift
@@ -11,6 +11,7 @@ class ProductsViewController: UIViewController {
     
     // MARK: Outlets
 
+    @IBOutlet weak var collectionView: UICollectionView!
     // MARK: Properties
         
     private let viewModel: ProductsViewModelType
@@ -30,9 +31,57 @@ class ProductsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpCollection()
+    }
+    
+    private func setUpCollection() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 }
 
+// MARK: - UICollectionViewDataSource
+extension ProductsViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+    }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension ProductsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+//MARK: UICollectionViewDelegateFlowLayout
+
+extension ProductsViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = (view.frame.width - 2) / 3
+        let width = view.frame.width
+        return CGSize(width: width, height: height)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 240)
+    }
+}
 // MARK: - Actions
 //
 extension ProductsViewController {
